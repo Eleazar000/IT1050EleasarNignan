4e3f370f547d94320c422f4e21c6dab3588cb456
Eleasar <S01209223@acad.tri-c.edu>
2018-10-11 00:49:11 -0400

    first comment

---
+++ LabW4.EleasarNignan1/LabW4.EleasarNignan1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabW4.EleasarNignan1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {/*
+            decimal dAmount1;
+            decimal dAmount2;
+            decimal dSum;
+            
+            Console.WriteLine("Enter the amount of first number:");
+            dAmount1 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the amount of second number:");
+            dAmount2 = decimal.Parse(Console.ReadLine());
+          
+            dSum = dAmount1 + dAmount2;
+            Console.WriteLine("The result 0f {0}+{1} is {2}", dAmount1, dAmount2, dSum);
+            
+
+            int x;
+            int y;
+            int z;
+            int dSum1;
+            int dSum2;
+            int dMult;
+
+            x = 2;
+            y = 6;
+            z = 1;
+
+            dSum1 = x + y;
+            dSum2 = z + 10;
+            dMult = dSum1 * dSum2;
+
+            dSum1 = x + y;
+            Console.WriteLine("The result 0f {0}+{1} is {2}", x, y, dSum1);
+
+            dSum2 = y + 10;
+            Console.WriteLine("The result 0f {0}+{1} is {2}", y, 10, dSum2);
+
+            dMult = dSum1*dSum2 ;
+            Console.WriteLine("The result 0f {0}*{1} is {2}", dSum1, dSum2, dMult);
+            */
+
+                Console.WriteLine("Hello\tWorld!");
+
+                Console.WriteLine("Hello\nWorld");
+
+                Console.WriteLine("\"Hello World!\"");
+
+                Console.WriteLine("Hello\\World!");
+
+
+            }
+        }
+    }
+}
