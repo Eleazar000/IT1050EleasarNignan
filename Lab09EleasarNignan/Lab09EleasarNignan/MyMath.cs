974f8cfc8db5038c61f3793d874e98fdb9e8da93
Eleasar <S01209223@acad.tri-c.edu>
2018-11-28 01:34:26 -0500

    Midterm

---
+++ Lab09EleasarNignan/Lab09EleasarNignan/MyMath.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09EleasarNignan
+{
+    class MyMath
+    {
+        public double result;
+        public double operand1;
+        public double operand2;
+
+        
+
+        public void Multiply(double operand1, double operand2)
+        {
+
+            {
+                this.result = operand1 * operand2;
+
+
+                {
+                    this.result = operand1 * operand2;
+                }
+
+                return;
+            }
+        }
+
+        public void Divide(double operand1, double operand2)
+        {
+
+            {
+                this.result = operand1 / operand2;
+
+
+                {
+                    this.result = operand1 / operand2;
+                }
+
+                return;
+            }
+        }
+        public void Substract(double operand1, double operand2)
+
+
+        {
+            this.result = operand1 - operand2;
+
+
+            {
+                this.result = operand1 - operand2;
+            }
+
+            return;
+        }
+        public void Add(double operand1, double operand2)
+        {
+
+            {
+                this.result = operand1 + operand2;
+
+
+                {
+                    this.result = operand1 + operand2;
+                }
+
+                return;
+            }
+        }
+
+
+    }    }
+
---
+++ Lab09EleasarNignan/Lab09EleasarNignan/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09EleasarNignan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath GetResult1 = new MyMath();
+            MyMath GetResult2 = new MyMath();
+            MyMath GetResult3 = new MyMath();
+            MyMath GetResult4 = new MyMath();
+
+            
+
+            GetResult1.Multiply(100, 2);
+            GetResult2.Divide(100, 2);
+            GetResult3.Substract(100, 2);
+            GetResult4.Add(100, 2);
+
+            Console.WriteLine("The result of multiplication is "+GetResult1.result);
+            Console.WriteLine("The result of division is " + GetResult2.result);
+            Console.WriteLine("The result of substraction is " + GetResult3.result);
+            Console.WriteLine("The result of addition is " + GetResult4.result);
+        }
+    }
+}
