bde11434c406a795fe1f234506b120b8d90e6cef
Eleasar <S01209223@acad.tri-c.edu>
2018-12-13 03:19:48 -0500

    Lab13

---
+++ OOP/OOP/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Car
+    {
+        private int Speed;
+        private string Make;
+        private string Model;
+        private int Year;
+
+        public Car(string make, string model, int year,int speed)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Speed = speed;
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public int SpeedUp()
+        {
+
+
+            { this.Speed++; }
+
+            return 0;
+        }
+
+        public int SlowDown()
+        {
+            if(Speed>0)
+
+            { this.Speed--; }
+
+            return 0;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(Year + " " + Make + " " + Model + " is going " + Speed + " MPH.");
+        }
+
+
+
+    }
+}
